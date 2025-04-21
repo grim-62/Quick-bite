8b2cd1a470b9f13cf021cb2a2105cba1457f5d0d
Yashdesh-mukh <yashdeshmukh621@gmail.com>
2025-04-21 14:00:40 +0530

    image

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -27,8 +27,8 @@ const Footer = () => {
         <div className="footer-content-right">
             <h2>GET IN TOUCH</h2>
             <ul>
-                <li>+1-212-456-7890</li>
-                <li>contact@tomato.com</li>
+                <li>+91-8542647835</li>
+                <li>contact@Quickbite.com</li>
             </ul>
         </div>
       </div>
