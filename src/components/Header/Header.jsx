dcc322542f42b3f63dc7376dc21c2f3b32f0faf0
Prashant Dhurve <prashantxu@gmail.com>
2025-05-02 16:52:08 +0530

    commit chage test 1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <header className='header'>
             <div className='header-contents'>
                 <h2>Order Your Favourite Food Here</h2>
-                <p>
+                <p style={{backgroundColor:"#000"}}>
                     Choose from a diverse menu featuring a delectable array of dishes
                     crafted with the finest ingredients and culinary expertise.
                 </p>
